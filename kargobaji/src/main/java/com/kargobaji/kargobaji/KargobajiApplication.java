2c4b3ffd2ef637cb6c2be356860467e821b04301
waple <bkspringmyj@gmail.com>
2025-04-07 10:54:10 +0900

    백엔드 기본 서버입니다.

---
+++ kargobaji/src/main/java/com/kargobaji/kargobaji/KargobajiApplication.java
@@ -0,0 +1,13 @@
+package com.kargobaji.kargobaji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KargobajiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KargobajiApplication.class, args);
+	}
+
+}
---
+++ kargobaji/src/test/java/com/kargobaji/kargobaji/KargobajiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kargobaji.kargobaji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KargobajiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
