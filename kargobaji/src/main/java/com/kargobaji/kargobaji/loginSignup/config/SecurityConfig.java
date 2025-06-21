a08702a00834936e660a874200686b1e19af29c4
JouEunPark <sjkwkwkw@naver.com>
2025-06-21 22:18:10 +0900

    cors update

--- kargobaji/src/main/java/com/kargobaji/kargobaji/loginSignup/config/SecurityConfig.java
+++ kargobaji/src/main/java/com/kargobaji/kargobaji/loginSignup/config/SecurityConfig.java
@@ -43,10 +43,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOrigins(List.of("http://localhost:8081"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(false);
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
